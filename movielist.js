e0d9a26dae0835e484f0836afad9f4f85f2ba939
Gavaskar0543 <gavaskaraeg@gmail.com>
2023-05-18 09:28:25 +0530

    fav movie section pending

--- movielist.js
+++ movielist.js
@@ -1,12 +1,19 @@
 
 var movieList =[];
+const APIKEY = 'api_key=da3a66d8d5e7b9b57503a1a79c697b38';
+const iMAGEURL = 'https://image.tmdb.org/t/p/w500';
+let moviesbox;
 
+const HOMEURL = `https://api.themoviedb.org/3/discover/movie?${APIKEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
 $.ajax({
     type: "get",
-    url: "https://api.themoviedb.org/3/movie/200?api_key=da3a66d8d5e7b9b57503a1a79c697b38",
+    url: HOMEURL,
     success: function (data) {
-       // console.log(data);
-        movieDisplay(data);
+      moviesbox =  data.results;
+      moviesbox.forEach(element => {
+        movieDisplay(element)
+      });
+      
     }
    
 });
@@ -16,24 +23,35 @@ function movieDisplay(movie){
     let li = document.createElement('li');
     li.classList.add('col-2');
     li.innerHTML = `
-    <div  class="d-flex justify-content-between  bg-black" style="margin-top:5%;" >
+    <div  class="d-flex justify-content-between  " style="margin-top:5%;" >
     <div class="row">
     <div class="movie-poste colr"style="height:300px;">
-    <a href="moviePage.html?id=${movie.id}"><img src= ${IMAGEURL+movie.poster_path} height="100%%" width="100%" alt="Movie Poster"></a>
+    <a href="moviePage.html?id=${movie.id}"><img src= ${iMAGEURL+movie.poster_path} height="100%%" width="100%" alt="Movie Poster"></a>
  </div>
  <span class="col movie-title" style="font-size:1.5rem;">${movie.title}</span>
  <div class="movie-element-tags col">
     <span class="movie-rating title-yellow">
+   
     <i class="fas fa-star"></i> ${movie.vote_average}
-    <i class="fa-sharp  fa-heart"></i>
-    </span>
+  </span>
+  <span id="favmovie" onclick="favmovie()"> <i class="fas fa-heart"></i></span>
     </div>
  </div>
      </div>`   
 /* `movieBox.append(li)` is appending the newly created `li` element to the `movieBox` element. This
 means that the `li` element will be added as a child element of the `movieBox` element. */
-.$("#showmovie").append(li);
+$("#showMovies").append(li);
  } 
+ 
+//fav
+let favmovie = () =>{
+  console.log("fav click")
+  let fav = $("#favmovie");
+  console.log(fav)
+}
+
+
+
 
 
  
--- moviepage.js
+++ moviepage.js
@@ -7,7 +7,8 @@ is used to extract the query string parameters from the URL, and the .get() meth
 retrieve the value of the 'id' parameter. */
 const movieId =  new URLSearchParams(window.location.search).get('id');
 console.log(movieId);
- 
+const iMAGEURL = 'https://image.tmdb.org/t/p/w500';
+
 $.ajax({
     type: "get",
     url: `https://api.themoviedb.org/3/movie/${movieId}?api_key=da3a66d8d5e7b9b57503a1a79c697b38`,
@@ -19,9 +20,16 @@ $.ajax({
 
 function movieDisplay(movie){
    let play = $("#movie-play");
+   let genres = movie.genres;
 let div = document.createElement('div');
   div.classList.add('movie-show');
   div.innerHTML = `
-  <h1 style="color:white">${movie.title}</h1>`
+  <div style="color:white;position:relative; height:10%; width:90%;margin-top:5%; margin-left:5%; border:1px solid white;">
+  <h1 style="color:white">${movie.title}</h1>
+  <img src="${iMAGEURL+movie.poster_path}" alt="img" height="100%" width="30%" >
+<div style="display:flex;margin-top:-40%; margin-left:35%;flex-direction:column;position:absolute; width:60%;">
+<h3>Overview</h3>
+<p>${movie.overview}</p>
+<h3>Release Date-${movie.release_date}</h3></div> </div>   `  
 play.append(div);
  } 
